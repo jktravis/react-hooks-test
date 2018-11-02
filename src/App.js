2beb13a0b18f3685c1339846409655c3dbcfd199
jktravis <joshua.travis@gmail.com>
2018-11-02 19:58:28 -0400

    Wired up class version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    firstName: '',
+    lastName: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <form>
+            <div className="first-name">
+              <label htmlFor="first-name">First Name</label>
+              <input name="firstName" id="first-name" value={this.state.firstName}
+                     onChange={this.handleChange}
+              />
+            </div>
+            <div className="last-name">
+              <label htmlFor="last-name">Last Name</label>
+              <input name="lastName" id="last-name" value={this.state.lastName}
+                     onChange={this.handleChange}
+              />
+            </div>
+          </form>
         </header>
       </div>
     );
