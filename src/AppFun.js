a911f0a38dc35a4a60f1e66c93437c71ee2339ee
jktravis <joshua.travis@gmail.com>
2018-11-02 20:10:22 -0400

    Add functional equivalent

---
+++ src/AppFun.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function AppFun() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const setters = {
+    setFirstName,
+    setLastName
+  };
+
+  function handleChange(event) {
+    setters[event.target.name](event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <div className="first-name">
+            <label htmlFor="first-name">First Name</label>
+            <input
+              name="setFirstName"
+              id="first-name"
+              value={firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="last-name">
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              name="setLastName"
+              id="last-name"
+              value={lastName}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default AppFun;
