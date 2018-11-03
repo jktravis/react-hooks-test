18d30f29acbfb6b518beed6d54191d68e900b7bb
jktravis <joshua.travis@gmail.com>
2018-11-02 20:25:29 -0400

    Add reducer

---
+++ src/AppReducer.js
@@ -0,0 +1,103 @@
+import React, { useReducer } from "react";
+import "./App.css";
+
+function AppReducer() {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'update/firstName':
+        return {
+          ...state,
+          firstName: action.payload,
+        };
+
+      case 'update/lastName':
+        return {
+          ...state,
+          lastName: action.payload,
+        };
+
+      default:
+        return state;
+    }
+  }, { firstName: '', lastName: '' });
+
+  const handleChange = (event) => {
+    dispatch({
+      type: event.target.name,
+      payload: event.target.value,
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <div className="first-name">
+            <label htmlFor="first-name">First Name</label>
+            <input
+              name="update/firstName"
+              id="first-name"
+              value={state.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="last-name">
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              name="update/lastName"
+              id="last-name"
+              value={state.lastName}
+              onChange={handleChange}
+            />
+          </div>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default AppReducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function useFirstLastNameReducer() {
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case 'update/firstName':
+        return {
+          ...state,
+          firstName: action.payload,
+        };
+
+      case 'update/lastName':
+        return {
+          ...state,
+          lastName: action.payload,
+        };
+
+      default:
+        return state;
+    }
+  }, { firstName: '', lastName: '' });
+
+  return [state, dispatch];
+}
+
