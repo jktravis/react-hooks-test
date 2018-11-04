6631ccc6452e665ab4e301a04cb0421b7f66e2db
jktravis <joshua.travis@gmail.com>
2018-11-04 11:31:28 -0500

    Convert hook state to counter ex

--- src/HookState.js
+++ src/HookState.js
@@ -2,42 +2,19 @@ import React, { useState } from "react";
 import "./App.css";
 
 function HookState() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  const setters = {
-    setFirstName,
-    setLastName
-  };
-
-  function handleChange(event) {
-    setters[event.target.name](event.target.value);
-  }
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Hook / State</p>
-        <form>
-          <div className="first-name">
-            <label htmlFor="first-name">First Name</label>
-            <input
-              name="setFirstName"
-              id="first-name"
-              value={firstName}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="last-name">
-            <label htmlFor="last-name">Last Name</label>
-            <input
-              name="setLastName"
-              id="last-name"
-              value={lastName}
-              onChange={handleChange}
-            />
-          </div>
-        </form>
+        <div className="counter">
+          {counter}
+        </div>
+        <div className="buttons">
+          <button onClick={() => setCounter(counter + 1)}>inc ☝</button>
+          <button onClick={() => setCounter(counter - 1)}>dec 👇</button>
+        </div>
       </header>
     </div>
   );
